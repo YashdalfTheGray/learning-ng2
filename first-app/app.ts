1765460c22cd90d80bfad2389d1f3d9edb073fa1
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-07-29 19:05:04 -0400

    added some interaction

--- first-app/app.ts
+++ first-app/app.ts
@@ -5,21 +5,28 @@ import {Component} from "@angular/core";
     selector: 'reddit',
     template: `
         <form class="ui large form segment">
-            <h3 class="ui header">
-                <div class="field">
-                    <label for="title">Title</label>
-                    <input type="text" name="title" />
-                </div>
-                <div class="field">
-                    <label for="link">Link</label>
-                    <input type="text" name="link" />
-                </div>
-            </h3>
+            <h3 class="ui header"></h3>
+            <div class="field">
+                <label for="title">Title</label>
+                <input type="text" name="title" #newTitle>
+            </div>
+            <div class="field">
+                <label for="link">Link</label>
+                <input type="text" name="link" #newLink>
+            </div>
+
+            <button className="ui positive right floated button" (click)="addArticle(newTitle, newLink)">
+                Submit Link
+            </button>
         </form>
     `
 })
 class RedditApp {
     constructor() {
+    }
+
+    addArticle(title: HTMLInputElement, link: HTMLInputElement): void {
+        console.log(`Addign article title: ${title.value} and link: ${link.value}.`);
     }
 }
 
