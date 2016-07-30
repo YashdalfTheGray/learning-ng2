dd4008f8f127b5365529fec20d7ad2c78d528061
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-07-30 18:07:50 -0400

    split out app into templates and added article component

--- first-app/app.ts
+++ first-app/app.ts
@@ -2,24 +2,38 @@ import {bootstrap} from "@angular/platform-browser-dynamic";
 import {Component} from "@angular/core";
 
 @Component({
-    selector: 'reddit',
-    template: `
-        <form class="ui large form segment">
-            <h3 class="ui header"></h3>
-            <div class="field">
-                <label for="title">Title</label>
-                <input type="text" name="title" #newTitle>
-            </div>
-            <div class="field">
-                <label for="link">Link</label>
-                <input type="text" name="link" #newLink>
-            </div>
+    selector: 'reddit-article',
+    templateUrl: './article.component.html',
+    host: {
+        class: 'row'
+    }
+})
+class ArticleComponent {
+    votes: number;
+    title: string;
+    link: string;
 
-            <button class="ui positive right floated button" (click)="addArticle(newTitle, newLink)">
-                Submit Link
-            </button>
-        </form>
-    `
+    constructor() {
+        this.title = 'Angular 2';
+        this.link = 'http://angular.io';
+        this.votes = 10;
+    }
+
+    voteUp() {
+        this.votes++;
+        return false;
+    }
+
+    voteDown() {
+        this.votes--;
+        return false;
+    }
+}
+
+@Component({
+    selector: 'reddit',
+    directives: [ArticleComponent],
+    templateUrl: './app.html'
 })
 class RedditApp {
     constructor() {
